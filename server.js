deb65e7bf682b5fc16f6c0842608ce52cd14de32
vazkido <vazkid02@gmail.com>
2025-03-17 12:11:57 -0400

    deleted db folder

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 import express from "express";
-import connectDB from "./db/db.js";
+
+import { User } from "./models/User.js";
 import dotenv from "dotenv";
 dotenv.config({ path: "./config/.env" });
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, { dbName: "restApi" });
+    console.log("Connected Successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const port = process.env.PORT || 3000;
 const app = express();
 connectDB();

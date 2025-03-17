1a4e28681e42e150a6e9d3816df6a4516b02592b
vazkido <vazkid02@gmail.com>
2025-03-17 12:42:50 -0400

    Edited server.js to async and await

--- server.js
+++ server.js
@@ -17,6 +17,37 @@ const port = process.env.PORT || 3000;
 const app = express();
 connectDB();
 
+app
+  .route("/")
+  .get(async (req, res) => {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (error) {
+      res.status(500).send("Error with Server");
+    }
+  })
+  .post(async (req, res) => {
+    const { name, email } = req.body;
+    try {
+      const exists = await User.findOne({
+        email,
+      });
+      if (exists) {
+        return res.status(400).send("User already exists");
+      }
+      await User.create({
+        name,
+        email,
+      });
+      res.send("User Created Successfully");
+    } catch (error) {
+      res.status(500).send("Server is down");
+    }
+  })
+  .put()
+  .delete();
+
 app.listen(port, () => {
   console.log("Server is Running Live");
 });

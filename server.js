9a93fda55aded3ee5aa1b8fb132db78b28b4732e
vazkido <vazkid02@gmail.com>
2025-03-21 18:49:04 -0400

    App.get was added

--- server.js
+++ server.js
@@ -50,24 +50,28 @@ app
     const { name, age, email } = req.body;
     try {
       if (!email || (!name && !age)) {
-        return res.status(400).send("Email and one other field is required")
-        
+        return res.status(400).send("Email and one other field is required");
       }
-      const user = await User.findOne({email});
+      const user = await User.findOne({ email });
       if (!user) {
         return res.status(404).send("User Not Found");
-      } 
-      user.name = name || user.name
-      user.age = age || user.age
+      }
+      user.name = name || user.name;
+      user.age = age || user.age;
 
-      await user.save()
-      res.send("user has been updated")
+      await user.save();
+      res.send("user has been updated");
     } catch (error) {
       res.send(500).send("Server is down" + error.message);
     }
+  });
+
+app
+  .get(`/test/:name`, (res, req) => {
+    const { name } = req.params;
+    res.send("Welcome To Rest Api " + name);
   })
 
-  
   .delete();
 
 app.listen(port, () => {
